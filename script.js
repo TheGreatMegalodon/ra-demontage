263b7e16a51dfeeea8878e220157896c3f9dca97
“TheGreatMegalodon” <atomes.600@gmail.com>
2025-03-02 15:36:41 +0100

    main push

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ function scrollToElement(id, tp=true) {
 function redirectToSite(url) {
     window.location.href = url;
 }
+
+var urlStatus = {
+    lastStatus: "",
+    check: function() {
+        const page = window.location.href.split('?=')[1];
+        document.querySelector('body').classList.toggle('visible');
+
+        if (this.lastStatus !== "") {
+            document.getElementById(this.lastStatus).classList.toggle('visible');
+        }
+        this.lastStatus = page;
+        document.getElementById(page).classList.toggle('visible');
+    },
+    update: function(location) {
+        history.pushState(null, '', `?=${location}`);
+        this.check();
+    }
+};
