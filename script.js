e3b7097f6e365ee6d8f47a8b0fa0d7ca666eaf4a
“TheGreatMegalodon” <atomes.600@gmail.com>
2025-03-02 20:04:32 +0100

    main push, traductions and bug corrections

--- script.js
+++ script.js
@@ -12,21 +12,43 @@ function redirectToSite(url) {
     window.location.href = url;
 }
 
+function sendEmail() {  // todo:   https://www.emailjs.com/
+    urlStatus.update('');
+    /*
+    emailjs.init("TON_USER_ID"); // Remplace par ton User ID
+    emailjs.send("TON_SERVICE_ID", "TON_TEMPLATE_ID", {
+        to_name: "Destinataire",
+        from_name: "Ton nom",
+        message: "Ceci est un test d'e-mail via EmailJS",
+        reply_to: "tonemail@example.com"
+    }).then(
+        response => console.log("E-mail envoyé avec succès !", response),
+        error => console.error("Erreur lors de l'envoi de l'e-mail", error)
+    );
+    */
+}
+
 var urlStatus = {
     lastStatus: "",
-    check: function() {
-        const page = window.location.href.split('?=')[1];
-        document.querySelector('body').classList.toggle('visible');
+    check: function(page) {
+        if (this.lastStatus !== "" || page) {
+            document.querySelector('body').classList.toggle('visible');
+        }
         if (this.lastStatus !== "") {
             document.getElementById(this.lastStatus).classList.toggle('visible');
         }
-        this.lastStatus = page;
-        if (typeof(page) == "string" && page.length >= 1) {
+        this.lastStatus = page||"";
+        if (page) {
             document.getElementById(page).classList.toggle('visible');
         }
     },
     update: function(location) {
         history.pushState(null, '', `?=${location}`);
-        this.check();
+        this.check(location);
+        if (location.length <= 1) {
+            location = window.location.href.split('?=')[1];
+            new URLSearchParams(window.location.search).delete("");
+            window.history.replaceState(null, "", window.location.origin + window.location.pathname);
+        }
     }
 };
