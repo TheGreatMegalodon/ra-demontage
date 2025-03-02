d066a1dce22410ccb07e6262f3d2689ff362a81e
“TheGreatMegalodon” <atomes.600@gmail.com>
2025-03-02 16:29:27 +0100

    correction d'affichage des éléments

--- script.js
+++ script.js
@@ -17,12 +17,13 @@ var urlStatus = {
     check: function() {
         const page = window.location.href.split('?=')[1];
         document.querySelector('body').classList.toggle('visible');
-
         if (this.lastStatus !== "") {
             document.getElementById(this.lastStatus).classList.toggle('visible');
         }
         this.lastStatus = page;
-        document.getElementById(page).classList.toggle('visible');
+        if (typeof(page) == "string" && page.length >= 1) {
+            document.getElementById(page).classList.toggle('visible');
+        }
     },
     update: function(location) {
         history.pushState(null, '', `?=${location}`);
